8273c06e4232d590ab3667be3e59b4ebe2a0415a
DrMaemi <55173646+DrMaemi@users.noreply.github.com>
2021-03-11 01:13:50 +0900

    2021-03-11

--- C++/필수50선/DP/2169_로봇조종하기.cpp
+++ C++/필수50선/DP/2169_로봇조종하기.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <climits>
 using namespace std;
+#define V vector
+#define MIN INT_MIN/2
 
 int N, M;
-int map[1001][1001];
-int CM[1001][1001][3];
-bool visited[1001][1001];
+V<V<int>> map;
+V<V<V<int>>> CM;
+V<V<bool>> visited;
 int dy[] = {0, 1, 0};
 int dx[] = {1, 0, -1};
-int _max = INT_MIN;
 
-// d : dir
-void dfs(int y, int x, int d, int& sum) {
-    if (y == N && x == M) {
-        _max = max(_max, sum+map[y][x]);
-        return;
-    }
-    sum += map[y][x];
-    if (!visited[y][x] && CM[y][x][d] < sum) {
+int dp(int y, int x, int d) {
+    if (y == N-1 && x == M-1)
+        return map[y][x];
+    if (CM[y][x][d] == MIN) {
         visited[y][x] = 1;
-        CM[y][x][d] = sum;
+        int _max = MIN;
         for (int i=0; i<3; i++) {
             int ny = y+dy[i], nx = x+dx[i];
-            if (0 < ny && ny <= N && 0 < nx && nx <= M)
-                dfs(ny, nx, i, sum);
+            if (0 <= ny && ny < N && 0 <= nx && nx < M && !visited[ny][nx])
+                _max = max(_max, dp(ny, nx, i));
         }
+        CM[y][x][d] = _max+map[y][x];
         visited[y][x] = 0;
     }
-    sum -= map[y][x];
+    return CM[y][x][d];
 }
 
 int main() {
     cin.tie(0); cout.tie(0);
     ios::sync_with_stdio(0);
     cin >> N >> M;
-    for (int i=1; i<=N; i++)
-        for (int j=1; j<=M; j++)
+    map.resize(N, V<int>(M));
+    visited.resize(N, V<bool>(M));
+    CM.resize(N, V<V<int>>(M, V<int>(3, MIN)));
+    for (int i=0; i<N; i++)
+        for (int j=0; j<M; j++)
             cin >> map[i][j];
-    fill(&CM[1][1][0], &CM[N][M][3], INT_MIN);
-    int sum = 0;
-    dfs(1, 1, 1, sum);
-    cout << _max;
+    cout << dp(0, 0, 0);
     return 0;
 }
