85b0fcab0053b8cf9cb8c249c0b7ad16840ebd64
DrMaemi <55173646+DrMaemi@users.noreply.github.com>
2021-03-11 23:04:06 +0900

    2021-03-11

--- C++/단계별문제/동적계획법2/2629_양팔저울.cpp
+++ C++/단계별문제/동적계획법2/2629_양팔저울.cpp
@@ -1,48 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 #define V vector
 
-int N;
-V<int> W;
+int N, M;
+V<int> A;
 V<V<bool>> visited;
 
-void dfs(int i, int t) {
-    if (i == N) return;
-    for (int k=-1; k<2; k++) {
-        int w = abs(t+k*W[i]);
-        if (!visited[i][w]) {
-            visited[i][w] = 1;
-            dfs(i+1, w);
-        }
+void dfs(int i, int w) {
+    if (i != N+1 && !visited[i][w]) {
+        visited[i][w] = 1;
+        dfs(i+1, w+A[i]);
+        dfs(i+1, w);
+        dfs(i+1, abs(w-A[i]));
     }
 }
 
 int main() {
-    ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
     cin >> N;
-    W.resize(N);
+    A.resize(N);
     int ws = 0;
     for (int i=0; i<N; i++) {
-        cin >> W[i];
-        ws += W[i];
+        cin >> A[i];
+        ws += A[i];
     }
-    visited.resize(N, V<bool>(ws+1));
+    visited.resize(N+1, V<bool>(ws+1));
     dfs(0, 0);
-    queue<int> q;
-    int M; cin >> M;
+    cin >> M;
     while (M--) {
         int n; cin >> n;
-        q.push(n);
-    }
-    while (!q.empty()) {
-        int nx = q.front(); q.pop();
-        if (nx > ws || !visited[N-1][nx])
-            cout << "N ";
+        if (ws < n || !visited[N][n])
+            cout << "N" << " ";
         else
-            cout << "Y ";
+            cout << "Y" << " ";
     }
     return 0;
 }
