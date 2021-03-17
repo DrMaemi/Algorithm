40ad09fd4c6c57a996aa593cc7fe9c2bdd5c0813
DrMaemi <55173646+DrMaemi@users.noreply.github.com>
2021-03-17 22:31:40 +0900

    2021-03-17

--- C++/어려움/2315_가로등끄기.cpp
+++ C++/어려움/2315_가로등끄기.cpp
@@ -15,14 +15,14 @@ V<V<V<ll>>> CM;
 ll dp(int l, int r, int f) {
     if (l == 1 && r == N)
         return 0;
-    if (CM[l][r][f] == MAX) {
+    if (!CM[l][r][f]) {
         ll c = CS[N]-CS[r]+CS[l-1];
         int d = f? r: l;
         ll _min = min(
             l-1? dp(l-1, r, 0)+abs(A[d].p-A[l-1].p)*c: MAX,
             r < N? dp(l, r+1, 1)+abs(A[d].p-A[r+1].p)*c: MAX
             );
-        CM[l][r][f] = min(CM[l][r][f], _min);
+        CM[l][r][f] = _min;
     }
     return CM[l][r][f];
 }
@@ -32,7 +32,7 @@ int main() {
     ios::sync_with_stdio(0);
     cin >> N >> M;
     A.resize(N+1); CS.resize(N+1);
-    CM.resize(N+1, V<V<ll>>(N+1, V<ll>(2, MAX)));
+    CM.resize(N+1, V<V<ll>>(N+1, V<ll>(2)));
     for (int i=1; i<=N; i++) {
         cin >> A[i].p >> A[i].c;
         CS[i] = CS[i-1]+A[i].c;
