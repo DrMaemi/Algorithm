70ab1ec70d480a50b7ff562887c29b28a4340c7f
DrMaemi <55173646+DrMaemi@users.noreply.github.com>
2021-03-30 15:59:16 +0900

    2021-03-30

--- Contest Rounds/711/C.cpp
+++ Contest Rounds/711/C.cpp
@@ -11,15 +11,16 @@ int main() {
     while (T--) {
         int N, K; cin >> N >> K;
         V<V<int>> CM(N+2, V<int>(K+2));
+        // CM[n][k] : n번째 plane에서 D(k)를 방출하는 횟수
         for (int i=0; i<=N+1; i++)
             CM[i][K] = 1;
         int ans = 1;
         for (int k=K-1; k>=1; k--)
-            if ((K-k) & 1) {
+            if ((K-k) & 1) { // D(k)가 역방향으로 진행하는 경우
                 CM[N][k] = CM[N-1][k+1];
-                for (int n=N-1; n>=1; n--)
+                for (int n=N-1; n>=1; n--) // 모든 plane에 대해 D(k) 누적합
                     CM[n][k] = (CM[n-1][k+1]+CM[n+1][k])%div;
-                ans = (ans+CM[1][k])%div;
+                ans = (ans+CM[1][k])%div; // 누적합이 CM[1][k]에 저장되어 있음.
             }
             else {
                 CM[1][k] = CM[2][k+1];
