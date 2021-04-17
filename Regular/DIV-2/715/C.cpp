e9dbd9ee00d6cef58ab7017c599cb1cd67778337
DrMaemi <leesh1510@ajou.ac.kr>
2021-04-18 01:04:59 +0900

    2021-04-18, #715 Problem C up-solving

--- Regular/DIV-2/715/C.cpp
+++ Regular/DIV-2/715/C.cpp
@@ -1,50 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <map>
 #include <algorithm>
 using namespace std;
-#define MAX 1000000000
+#define MAX 2000
 typedef long long ll;
-struct P { int n, cnt; };
-struct PP { int n, d; };
 
-bool cmp(P& _1, P& _2) { return _1.cnt > _2.cnt; }
-bool cmpp(PP& _1, PP& _2) { return _1.d < _2.d; }
+ll CM[MAX][MAX], A[MAX];
+
+ll dp(int l, int r) {
+    if (l == r) return 0;
+    if (CM[l][r] == -1)
+        CM[l][r] = A[r]-A[l]+min(dp(l, r-1), dp(l+1, r));
+    return CM[l][r];
+}
 
 int main() {
     int N; cin >> N;
-    map<int, int> m;
-    for (int i=0; i<N; i++) {
-        int n; cin >> n;
-        if (m.find(n) == m.end()) m[n] = 1;
-        else m[n]++;
-    }
-    vector<P> V;
-    for (auto it=m.begin(); it!=m.end(); it++)
-        V.push_back({it->first, it->second});
-    sort(V.begin(), V.end(), cmp);
-    ll ans = 0; int i = 0, _max = -1, _min = -1;
-    while (i < V.size()) {
-        int cnt = V[i].cnt;
-        vector<int> vv(1, V[i++].n);
-        while (i < V.size())
-            if (V[i].cnt != cnt) break;
-            else vv.push_back(V[i++].n);
-        sort(vv.begin(), vv.end());
-        
-        vector<PP> vvv;
-        for (int x: vv) {
-            int d = x > _max? x-_max: 0 + x < _min? _min-x: 0;
-            vvv.push_back({x, d});
-        }
-        sort(vvv.begin(), vvv.end(), cmpp);
-        for (PP pp: vvv) cout << pp.n << " ";
-        if (_max == -1) { _max = _min = vvv[0].n; }
-        for (PP pp: vvv) { 
-            _max = max(_max, pp.n); _min = min(_min, pp.n);
-            ans += (_max-_min)*cnt;
-        }
-    }
-    cout << ans << "\n";
+    for (int i=0; i<N; i++) cin >> A[i];
+    sort(A, A+N);
+    fill(&CM[0][0], &CM[N-1][N], -1);
+    cout << dp(0, N-1);
     return 0;
 }
